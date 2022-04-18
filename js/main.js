7d7020721aabc5368aee1469a4dd9b93a9f40fdc
voronkov1988 <rybsoveti.ru@yandex.ru>
2022-04-19 02:36:10 +0300

    ready 1.5

--- js/main.js
+++ js/main.js
@@ -1,63 +1,48 @@
-'use strict';
-
 const readButton = document.querySelector('.open')
 const closeButton = document.querySelector('.close')
 const allSlides = document.querySelectorAll('.slider__oneSlider')
 
+const medium = document.querySelectorAll('.slider__oneSlider:nth-child(n+7)')
+const large = document.querySelectorAll('.slider__oneSlider:nth-child(n+9')
+
+let swiper
+
 document.addEventListener("DOMContentLoaded", ()=> {
-    if (window.screen.width <= 768) {
-        console.log(`width ${window.screen.width}`)
-        readButton.style.display = 'none'
-        closeButton.style.display = 'none'
-    }
-    if (window.screen.width >= 768 && window.screen.height < 1120) {
-        allSlides.forEach((slide, i) => {
-            if (i >= 6) {
-                slide.classList.add('slide__slide_hidden')
-                readButton.style.display = 'flex'
-                closeButton.style.display = 'none'
-            }
-        })
-    }
-    if (window.screen.width > 1119) {
-        allSlides.forEach((slide, i) => {
-            slide.classList.remove('slide__slide_hidden')
-            if (i >= 8) {
-                console.log(slide)
-                slide.classList.add('slide__slide_hidden')
-                readButton.style.display = 'flex'
-                closeButton.style.display = 'none'
-            }
-        })
+    if (window.screen.width < 768) {
+        createSwiper()
     }
 });
 
-
-readButton.addEventListener('click', () => {
-    allSlides.forEach(slide => {
-        slide.classList.remove('slide__slide_hidden')
-        readButton.style.display = 'none'
-        closeButton.style.display = 'flex'
-    })
-})
-
-closeButton.addEventListener('click', () => {
-    allSlides.forEach((slide, i) => {
-        if (i == 6) {
-            slide.classList.add('slide__slide_hidden')
-        }
-        if (i < 6) {
-            console.log(i)
-            slide.classList.remove('slide__slide_hidden')
-        }
-        if(i > 8) {
-            console.log(i)
-            slide.classList.remove('slide__slide_hidden')
-        }
-        closeButton.style.display = 'none'
+const resizeHandlerSlider = () => {
+    if (document.body.clientWidth >= 768 && document.body.clientWidth < 1120) {
         readButton.style.display = 'flex'
-    })
-})
+        closeButton.style.display = 'none'
+        medium.forEach (item => {
+            item.style.display = 'none'
+        })
+        if (swiper) {
+            swiper.destroy();
+        }
+    }
+    if(document.body.clientWidth < 768){
+        createSwiper()
+        medium.forEach (item => {
+            item.style.display = 'flex'
+        })
+        readButton.style.display = 'none'
+        closeButton.style.display = 'none'
+    }
+    if(document.body.clientWidth > 1120){
+        readButton.style.display = 'flex'
+        closeButton.style.display = 'none'
+        medium.forEach (item => {
+            item.style.display = 'flex'
+        })
+        large.forEach (item => {
+            item.style.display = 'none'
+        })
+    }
+}
 
 const debounce = (func, delay) => {
     let debounceTimer
@@ -70,32 +55,47 @@ const debounce = (func, delay) => {
     }
 }
 
+const createSwiper = () => {
+    swiper = new Swiper('.slider__wrapper', {
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.slider__dots',
+            clickable: true,
+        }
+    })
+}
 
 
-window.addEventListener('resize', debounce(function () {
-    if (window.screen.width <= 768) {
-        console.log(`width ${window.screen.width}`)
+readButton.addEventListener('click', () => {
+    if(window.screen.width < 1120){
+        medium.forEach (item => {
+            item.style.display = 'flex'
+        })
+    }
+    
+    large.forEach (item => {
+        item.style.display = 'flex'
+    })
+    allSlides.forEach((slide,i) => {
         readButton.style.display = 'none'
-        closeButton.style.display = 'none'
-    }
-    if (window.screen.width >= 768 && window.screen.height < 1120) {
-        allSlides.forEach((slide, i) => {
-            if (i >= 6) {
-                slide.classList.add('slide__slide_hidden')
-                readButton.style.display = 'flex'
-                closeButton.style.display = 'none'
-            }
+        closeButton.style.display = 'flex'
+    })
+})
+
+closeButton.addEventListener('click', () => {
+    if(window.screen.width < 1120){
+        medium.forEach (item => {
+            item.style.display = 'none'
+        })
+    }else {
+        large.forEach (item => {
+            item.style.display = 'none'
         })
     }
-    if (window.screen.width > 1119) {
-        allSlides.forEach((slide, i) => {
-            slide.classList.remove('slide__slide_hidden')
-            if (i >= 8) {
-                console.log(slide)
-                slide.classList.add('slide__slide_hidden')
-                readButton.style.display = 'flex'
-                closeButton.style.display = 'none'
-            }
-        })
-    }
-}, 500))
+    
+    closeButton.style.display = 'none'
+    readButton.style.display = 'flex'
+})
+
+
+window.addEventListener('resize', debounce(resizeHandlerSlider, 200))
